80e49f2c5ad12c05814d77e3a5da25fe9bd82a79
christeffer <christeffer@hotmail.com>
2020-05-06 14:29:13 -0300

    Adding project

---
+++ src/components/MineField.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Field from './Field';
+
+export default (props) => {
+  const rows = props.board.map((row, r) => {
+    const columns = row.map((field, c) => {
+      return (
+        <Field
+          {...field}
+          key={c}
+          onOpen={() => props.onOpenField(r, c)}
+          onSelect={(e) => props.onSelectField(r, c)}
+        />
+      );
+    });
+    return (
+      <View key={r} style={styles.viewMine}>
+        {columns}
+      </View>
+    );
+  });
+  return <View style={styles.container}>{rows}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EEE',
+  },
+  viewMine: {
+    flexDirection: 'row',
+  },
+});
